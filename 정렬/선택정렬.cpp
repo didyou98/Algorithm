4217013df31183587231c3f1743f797d98a1d285
Yeonjae Sung <88304798+didyou98@users.noreply.github.com>
2021-11-30 21:31:44 +0900

    Update 선택정렬.cpp

--- 정렬/선택정렬.cpp
+++ 정렬/선택정렬.cpp
@@ -10,17 +10,20 @@ int main(void)
 	
 	for(int i = 0; i < numsSize; i++)
 	{
-		minIdx = i;
+		minIdx = i; //0을 초기 인덱스로 설정
 		for(int j = i + 1; j < numsSize; j++)
 		{
+			// i와 i + 1을 비교해서 i번째 요소가 더 크다면
 			if(nums[minIdx] > nums[j])
 			{
+				// minIdx를 갱신
 				minIdx = j;
 			}
 		}
+		// swap을 통해 숫자를 서로 바꾼다
 		swap(nums[i], nums[minIdx]);
 	}
-	
+	// 
 	for(int i = 0; i < numsSize; i++)
 	{
 		cout << nums[i] << " ";
