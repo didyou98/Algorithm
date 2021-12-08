ed5c0f1c572154d31b9f7fd755065306b053cd52
didyou98 <didyou98@naver.com>
2021-12-08 19:38:31 +0900

    commit

--- 다이나믹프로그래밍/1로 만들기.cpp
+++ 다이나믹프로그래밍/1로 만들기.cpp
@@ -33,8 +33,7 @@
 using namespace std;
 
 int x;
-int d[30000];
-int cnt;
+int d[1000001];
 
 int main(void)
 {
@@ -46,12 +45,8 @@ int main(void)
 			d[i] = min(d[i], d[i / 2] + 1);
 		if(i % 3 == 0)
 			d[i] = min(d[i], d[i / 3] + 1);
-		if(i % 5 == 0)
-			d[i] = min(d[i], d[i / 5] + 1);
-			
-		cout << d[i] << endl;
 	}
 	
-	cout << d[x] << '\n';
+	cout << d[x];
 }
  
