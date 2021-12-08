5d87d73d35a3d7980ab68d5d2fa35d99ac9a72f3
didyou98 <didyou98@naver.com>
2021-12-08 23:26:23 +0900

    commit

--- gitTest.cpp
+++ gitTest.cpp
@@ -1,12 +1,9 @@
-#include <bits/stdc++.h>
-
+#include <iostream>
 using namespace std;
+int dp[5001]; //global 변수이기때문에 0으로 초기화된 배열
 
-int main(void)
-{
-	int n = 3;
-	cout << n << endl;
-	cout << to_string(n) << endl;
-	string c = to_string(n);
-	if('3' == '33') cout << "same";
+int main() {
+	dp[1] = 1;
+	if(dp[6 - 5]) cout << "hi";
+	else cout << "hello";
 }
