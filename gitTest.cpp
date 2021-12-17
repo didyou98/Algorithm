28fae160253e5a1d124f193de9e32f31d82245de
didyou98 <didyou98@naver.com>
2021-12-17 10:13:33 +0900

    commit

--- gitTest.cpp
+++ gitTest.cpp
@@ -1,8 +1,30 @@
+/*
+	게임 개발 
+	작성일: 2021.12.05
+	작성자 : 성연재
+	작성 언어 : C++
+	
+	문제
+
+	 
+	  
+	 
+	입력조건 
+	 	 
+	출력 조건 
+	
+	
+	입력예시
+	
+	
+	출력예시
+*/
+
 #include <bits/stdc++.h>
 
 using namespace std;
 
 int main(void)
 {
-	cout << abs(1 - 2);
+	cout << 4200 % 1000;
 }
